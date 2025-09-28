e18173183e2ed4e1fb8f19c3edaf75d9e6cfa4c4
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2025-09-28 11:39:26 -0600

    first commit

---
+++ src/main/java/unam/dgtic/mvc_componentes/MvcComponentesApplication.java
@@ -0,0 +1,13 @@
+package unam.dgtic.mvc_componentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcComponentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unam/dgtic/mvc_componentes/MvcComponentesApplicationTests.java
@@ -0,0 +1,13 @@
+package unam.dgtic.mvc_componentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcComponentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
